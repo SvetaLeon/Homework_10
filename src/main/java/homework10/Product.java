908f90d08c258021bf0669cb5e818cc2e2840765
Shutko Svitlana <svetalysenko25@gmail.com>
2024-01-16 19:07:09 +0200

    added homework 10 Stream API

---
+++ src/main/java/homework10/Product.java
@@ -0,0 +1,19 @@
+package homework10;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@ToString
+public class Product {
+    private final String type;
+    private final double price;
+    private final boolean discount;
+    private final LocalDateTime dateOfAddition;
+}
